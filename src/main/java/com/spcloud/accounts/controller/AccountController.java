1216cbc3f5e4740da967e1d1d28b2c91518b100a
paras <parasmani300@gmail.com>
2024-01-07 15:02:03 +0530

    Intital commit

---
+++ src/main/java/com/spcloud/accounts/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.spcloud.accounts.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    @Value("${text.info}")
+    String sampleText;
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> getHi()
+    {
+        String val = "Hello" + sampleText;
+        return new ResponseEntity<String>(val, HttpStatus.OK);
+    }
+}
